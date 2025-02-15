fc79913545b81082f4dee39c114a5ee4cbe91f1a
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-02-15 02:05:15 -0300

    feat(backend): implement mysql pool

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
+});
 
 const app = express();
 
