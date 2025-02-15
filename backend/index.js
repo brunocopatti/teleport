fe21d2a7df844d769bc8ac9cf5b205fa02cc012e
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-02-15 18:51:40 -0300

    feat(backend): generate redirect report

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,18 @@ app.get("/:shortPath", async (req, res, next) => {
 	try {
 		const { shortPath } = req.params;
 		const [results] = await pool.query(
-			"SELECT `destination_url` FROM `redirects` WHERE `short_path` = ?",
+			"SELECT `id`, `destination_url` FROM `redirects` WHERE `short_path` = ?",
 			[shortPath]
 		);
 		if (results.length !== 1) {
 			return res.sendStatus(404);
 		}
+		const redirectId = results[0].id;
 		const destinationUrl = results[0].destination_url;
+		await pool.execute(
+			"INSERT INTO `redirect_reports`(`redirect_id`, `ip_address`) VALUES (?, ?)",
+			[redirectId, req.ip]
+		);
 		return res.redirect(destinationUrl);
 	} catch (error) {
 		return next(error);
